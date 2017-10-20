3277bf75b7b640ba303a3594b1b2445ea6ebc518
okx-code <me@oliverallen.co.uk>
2017-10-20 21:04:42 +0100

    update version number

--- src/main/java/de/sabbertran/proxysuite/commands/ProxySuiteCommand.java
+++ src/main/java/de/sabbertran/proxysuite/commands/ProxySuiteCommand.java
@@ -17,52 +17,50 @@ public class ProxySuiteCommand extends Command {
 
     @Override
     public void execute(final CommandSender sender, final String[] args) {
-        main.getCommandHandler().executeCommand(sender, "proxysuite", new Runnable() {
-            public void run() {
-                if (args.length == 1) {
-                    if ((args[0].equalsIgnoreCase("reloadmessages") || args[0].equalsIgnoreCase("reloadmsg"))) {
-                        if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadmessages")) {
-                            main.getMessageHandler().readMessagesFromFile();
-                            main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("messages.reload.success"));
-                        } else {
-                            main.getPermissionHandler().sendMissingPermissionInfo(sender);
-                        }
-                    } else if ((args[0].equalsIgnoreCase("reloadannouncements") || args[0].equalsIgnoreCase("reloadann"))) {
-                        if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadannouncements")) {
-                            main.getAnnouncementHandler().readAnnouncementsFromFile();
-                            main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("announcements.reload.success"));
-                        } else {
-                            main.getPermissionHandler().sendMissingPermissionInfo(sender);
-                        }
-                    } else if (args[0].equalsIgnoreCase("reloadperms")) {
-                        if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadperms")) {
-                            main.getPermissionHandler().resetPermissions();
-                            main.getPermissionHandler().updatePermissions();
-                            main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("permissions.reload.success"));
-                        } else {
-                            main.getPermissionHandler().sendMissingPermissionInfo(sender);
-                        }
+        main.getCommandHandler().executeCommand(sender, "proxysuite", () -> {
+            if (args.length == 1) {
+                if ((args[0].equalsIgnoreCase("reloadmessages") || args[0].equalsIgnoreCase("reloadmsg"))) {
+                    if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadmessages")) {
+                        main.getMessageHandler().readMessagesFromFile();
+                        main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("messages.reload.success"));
+                    } else {
+                        main.getPermissionHandler().sendMissingPermissionInfo(sender);
                     }
-                } else if (args.length == 2) {
-                    if (args[0].equalsIgnoreCase("reloadperms")) {
-                        ProxiedPlayer p = main.getPlayerHandler().getPlayer(args[1], sender, true);
-                        if (p != null) {
-                            if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadperms")) {
-                                main.getPermissionHandler().resetPermissions(p);
-                                main.getPermissionHandler().updatePermissions(p);
-                                main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("permissions.reload.success.player").replace("%player%", p.getName()));
-                            } else {
-                                main.getPermissionHandler().sendMissingPermissionInfo(sender);
-                            }
+                } else if ((args[0].equalsIgnoreCase("reloadannouncements") || args[0].equalsIgnoreCase("reloadann"))) {
+                    if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadannouncements")) {
+                        main.getAnnouncementHandler().readAnnouncementsFromFile();
+                        main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("announcements.reload.success"));
+                    } else {
+                        main.getPermissionHandler().sendMissingPermissionInfo(sender);
+                    }
+                } else if (args[0].equalsIgnoreCase("reloadperms")) {
+                    if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadperms")) {
+                        main.getPermissionHandler().resetPermissions();
+                        main.getPermissionHandler().updatePermissions();
+                        main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("permissions.reload.success"));
+                    } else {
+                        main.getPermissionHandler().sendMissingPermissionInfo(sender);
+                    }
+                }
+            } else if (args.length == 2) {
+                if (args[0].equalsIgnoreCase("reloadperms")) {
+                    ProxiedPlayer p = main.getPlayerHandler().getPlayer(args[1], sender, true);
+                    if (p != null) {
+                        if (main.getPermissionHandler().hasPermission(sender, "proxysuite.commands.reloadperms")) {
+                            main.getPermissionHandler().resetPermissions(p);
+                            main.getPermissionHandler().updatePermissions(p);
+                            main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("permissions.reload.success.player").replace("%player%", p.getName()));
                         } else {
-                            main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("command.player.notseen").replace("%player%", args[1]));
+                            main.getPermissionHandler().sendMissingPermissionInfo(sender);
                         }
                     } else {
-                        main.getCommandHandler().sendUsage(sender, self);
+                        main.getMessageHandler().sendMessage(sender, main.getMessageHandler().getMessage("command.player.notseen").replace("%player%", args[1]));
                     }
                 } else {
-                    main.getMessageHandler().sendMessage(sender, "&6Proxy&8Suite &r" + main.getDescription().getVersion() + " by &b" + main.getDescription().getAuthor());
+                    main.getCommandHandler().sendUsage(sender, self);
                 }
+            } else {
+                main.getMessageHandler().sendMessage(sender, "&6Proxy&8Suite &r" + main.getDescription().getVersion() + " by &b" + main.getDescription().getAuthor());
             }
         });
     }
